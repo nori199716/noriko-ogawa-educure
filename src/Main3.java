092f73bbf56b70cc19699c53859a3d300a4df04c
nori199716 <emma.111416@gmail.com>
2026-02-20 03:34:55 +0900

    Add files via upload

---
+++ src/Main3.java
@@ -0,0 +1,16 @@
+/**
+ * これはHello,Worldを表示するプログラムです。
+ * Javaのコメントの書き方を学んでいます。
+ */
+public class Main3 {
+
+    /**
+     * このプログラムは
+     * コンソールにHello,World!と表示します。
+    */
+   public static void main(String[] args){
+
+    // 画面に文字を出力する
+    System.out.println("Hello, World!");
+   }
+}
---
+++ src/Main8.java
@@ -0,0 +1,20 @@
+/**
+ * このプログラムは、簡単な自己紹介を表示しています。
+ * 名前・出身地・好きな食べ物を表示します。
+ */
+public class Main8 {
+    public static void main(String[] args) {
+        //自己紹介の情報を変数に代入
+        String name="山田太郎";
+        String birthplace="東京都";
+        String favoritefood="カレーライス";
+
+        //区切り線を表示
+        System.out.println("==== 自己紹介 ====");
+
+        //各項目を表示
+        System.out.println("名前："+name);
+        System.out.println("出身地："+birthplace);
+        System.out.println("好きな食べ物："+favoritefood);
+    }
+}
