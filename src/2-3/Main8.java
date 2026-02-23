553ed3cae80dcb9ff2d06a926aec61f2eb188237
nori199716 <emma.111416@gmail.com>
2026-02-24 06:47:44 +0900

    Update Main8.java

--- src/2-3/Main8.java
+++ src/2-3/Main8.java
@@ -7,7 +7,7 @@ public class Main8 {
         //自己紹介の情報を変数に代入
         String name="山田太郎";
         String birthplace="東京都";
-        String favoritefood="カレーライス";
+        String favoriteFood="カレーライス";
 
         //区切り線を表示
         System.out.println("==== 自己紹介 ====");
@@ -15,6 +15,7 @@ public class Main8 {
         //各項目を表示
         System.out.println("名前："+name);
         System.out.println("出身地："+birthplace);
-        System.out.println("好きな食べ物："+favoritefood);
+        System.out.println("好きな食べ物："+favoriteFood);
     }
 }
+
