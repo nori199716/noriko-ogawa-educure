c5d4947147d3266c66fc6208faa437a39ce8c482
nori199716 <emma.111416@gmail.com>
2026-03-04 10:09:35 +0900

    Update Main2.java

--- 2-7/Main2.java
+++ 2-7/Main2.java
@@ -3,15 +3,16 @@ package src;
 public class Main2 {
     
     public static void main(String[] args){
-        double price = 2500;
+        int price = 2500;
         double discountRate = 0.2;
 
-        double discount = price*discountRate;
-        double finalPrice = price-discount;
+        int discount = (int)(price*discountRate);
+        int finalPrice = price - discount;
 
         System.out.println("割引額"+discount+"円");
         System.out.println("販売価格"+finalPrice+"円");
 
     }
 }
+
 
