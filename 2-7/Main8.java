3d5c84c0a01770e8b8616c74d4c7f7c8ba4c1537
nori199716 <emma.111416@gmail.com>
2026-03-04 10:15:45 +0900

    Update Main8.java

--- 2-7/Main8.java
+++ 2-7/Main8.java
@@ -8,10 +8,12 @@ public class Main8 {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("攻撃力を入力してください");
-        int attack = sc.nextInt();
+        String attackInput = sc.nextLine();
+        int attack = Integer.parseInt(attackInput);
 
         System.out.print("防御力を入力してください");
-        int defense = sc.nextInt();
+        String defenseInput = sc.nextLine();
+        int defense = Integer.parseInt(defenseInput);
 
         int normalDamage = attack - defense;
         double critical = normalDamage * 1.5;
@@ -23,3 +25,4 @@ public class Main8 {
         sc.close();
     }
 }
+
