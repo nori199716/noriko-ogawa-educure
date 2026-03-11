2e91715fe7869c14a5d4912331450a2e9dd68387
nori199716 <emma.111416@gmail.com>
2026-03-12 08:25:53 +0900

    Add files via upload

---
+++ 2-9/Main1.java
@@ -0,0 +1,16 @@
+
+
+public class Main1 {
+    public static void main(String[] args){
+
+        int age = 18; //ここに年齢を入力
+
+        if (age >=20) {
+            System.out.println("お酒を購入できます。");
+        }
+        else{
+            System.out.println("お酒を購入できません。");
+        }
+    }
+    
+}
