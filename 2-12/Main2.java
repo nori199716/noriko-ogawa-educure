0996e5531ea7f6f113332ed02dfedc71860f7b23
nori199716 <emma.111416@gmail.com>
2026-03-14 23:00:04 +0900

    Add files via upload

---
+++ 2-12/Main2.java
@@ -0,0 +1,14 @@
+
+public class Main2 {
+    public static void main(String[] args){
+
+        for (int dan =1; dan <= 5; dan++){
+            for(int kakezan = 1; kakezan <= 9; kakezan++){
+
+                System.out.println(dan +"×" + kakezan + "=" + (dan * kakezan));
+            }
+        }
+
+        
+    }
+}
