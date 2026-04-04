7601123f6ddb6332c5cf184971a3842f050edc0a
nori199716 <emma.111416@gmail.com>
2026-04-04 21:13:37 +0900

    Add files via upload

---
+++ 2-18/Main4.java
@@ -0,0 +1,26 @@
+public class Main4 {
+    
+    public static void main(String[] srgs){
+        int[] prices = {100, 200, 300};
+        int[] quantities = {1, 2, 3};
+
+        int[] results = calcTotals(prices, quantities);
+
+         System.out.print("合計金額：");
+         for (int i = 0; i < results.length; i++){
+            System.out.print(results[i] + "　");
+         }
+    }
+
+    public static int[] calcTotals(int[] prices, int[] quantities){
+        int[] totals = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++){
+            int total = prices[i] * quantities[i];
+            total = (int)(total * 1.1);
+            totals[i] = total;
+        }
+
+        return totals;
+    }
+}
